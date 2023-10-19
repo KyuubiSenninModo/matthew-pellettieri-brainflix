ef80f74f77c073a55e7f5e02ef089d24bd288a9c
Matthew Pellettieri <mrpellettieri@gmail.comgit config --global user.email mrpellettieri@gmail.com>
2023-10-18 22:53:19 -0700

    Sprint-two submission Final

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import './App.scss';
-import { BrowserRouter, Routes, Route, } from 'react-router-dom';
-import VideoDetailsPage from './components/VideoDetailsPage/videoDetailsPage';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import "./App.scss";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import VideoDetailsPage from "./components/VideoDetailsPage/videoDetailsPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Upload from "./pages/Upload/upload";
 
 function App() {
-    const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]);
 
-    useEffect(() => {
-        axios
-            .get('https://project-2-api.herokuapp.com/videos?api_key=' + apiKey)
-            .then((res) => {
-                setVideos(res.data);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get("https://project-2-api.herokuapp.com/videos?api_key=" + apiKey)
+      .then((res) => {
+        setVideos(res.data);
+      });
+  }, []);
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<VideoDetailsPage videos={videos} />}>
-                    <Route path='/:id' element={<VideoDetailsPage videos={videos} />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route exact path="/" element={<VideoDetailsPage videos={videos} />}>
+            <Route path="/:id" element={<VideoDetailsPage videos={videos} />} />
+          </Route>
+          <Route path="/upload" element={<Upload />}> </Route> 
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
-export const apiKey = 'edbed7ac-b4d8-4be6-9373-68e93be6ff38';
+export const apiKey = "edbed7ac-b4d8-4be6-9373-68e93be6ff38";
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,24 +4,29 @@ import avatar from '../../assets/Images/Mohan-muruge.jpg';
 import { Link } from 'react-router-dom';
 
 function Header() {
+    
     return (
         <header className='header'>
-            <Link to={'/'}>
+            <Link className='Link' to={'/'}>
                 <div className='logo--container'>
                     <img className='header__logo' src={logo} alt='BrainFlix-logo' />
                 </div>
             </Link>
             <div className='searchbar--container'>
-            <Link to={'/'}>
+            <Link className='Link' to={'/'}>
                 <img className='header__logo--tablet' src={logo} alt='BrainFlix-logo' />
             </Link>
                 <input type='text' className='searchbar' placeholder='Search' />
+                <Link className='Link' to={'/upload'}>
                 <button className='upload--button--tablet'>UPLOAD</button>
+                </Link>
                 <img className='avatar' src={avatar} alt='avatar' />
             </div>
+            <Link className='Link' to={'/upload'}>
             <div className='button--container'>
                 <button className='upload--button--mobile'>UPLOAD</button>
             </div>
+            </Link>
         </header>
     );
 }
